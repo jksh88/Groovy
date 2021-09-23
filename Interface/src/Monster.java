ba6ba1f8ef228b04daa9f17ba4f234f1a9c93842
Joohoon Kim <jayjhkim7@gmail.com>
2021-09-22 22:48:37 -0400

    interface wip

---
+++ Interface/src/Monster.java
@@ -0,0 +1,23 @@
+public class Monster {
+    String name;
+    int hitPoints;
+    int strength;
+
+    public Monster(String name, int hitPoints, int strength) {
+        this.name = name;
+        this.hitPoints = hitPoints;
+        this.strength = strength;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+}
---
+++ Interface/src/Player.java
@@ -0,0 +1,59 @@
+import java.util.List;
+
+public class Player implements  ISaveable {
+    String name;
+    String weapon;
+    int hitPoints;
+    int strength;
+
+    public Player(String name, int hitPoints, int strength) {
+        this.name = name;
+        this.hitPoints = hitPoints;
+        this.strength = strength;
+        this.weapon = "Sword";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWeapon() {
+        return weapon;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setWeapon(String weapon) {
+        this.weapon = weapon;
+    }
+
+    public void setHitPoints(int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public List<String> write() {
+
+    }
+
+    public read (List<String>) {
+
+    }
+
+    public String toString() {
+
+    }
+}
