71b0342d993f7223f9eae2f491a6f5fed7c0129e
Joohoon Kim <jayjhkim7@gmail.com>
2021-10-06 21:38:58 -0400

    continuing syntax

--- groovy-retry/src/retry.groovy
+++ groovy-retry/src/retry.groovy
@@ -1,2 +1,38 @@
-println "retry groovy"
-println "jar file added"
+multiLineStr = '''
+Mutlieline
+String example
+'''
+name = "jk"
+def employees = ["J": 48, "AM": 32, "PG":42]
+float a = 2.24F;
+int[] arr = [11,22,33,44,55]
+for(int i = 0; i <= 5; i++) {
+    if (i - a <= 0) {
+        continue;
+    }
+        println i;
+}
+for (int i in arr) {
+    String first = i.toString().split('')[0];
+    String second = i.toString().split('')[1];
+//    println(first)
+    assert first.toInteger() == second.toInteger();
+}
+
+for (String emp in employees) {
+    if(employees.get(emp) > 45) {
+        println(emp)
+    }
+    println(employees.get(emp))
+}
+print(employees.get("J"))
+
+print a
+println a.class
+nameStr = "my initials: ${name}"
+println nameStr
+println multiLineStr
+
+
+
+
