a19f4de8cbf1b31e69accb3d6b786034608ecc7f
Joohoon Kim <jayjhkim7@gmail.com>
2021-10-11 22:55:08 -0400

    groovy wip methods

--- groovy-retry/src/ClosuresPractice.groovy
+++ groovy-retry/src/ClosuresPractice.groovy
@@ -1,8 +1,15 @@
+lst = [10,20,30,30, 30, 33, 40];
 c={ n->
     println(n%2==0?"even":"odd")
 }
 
-c.call(15)
-
-5.times {x -> println x + 3}
-2.times{println it}
+//c.call(15)
+//
+//5.times {x -> println x + 3}
+//2.times{println it}
+//lst.find(c)
+println(lst.find{it > 26})
+newLst = lst.unique(false)
+println newLst;
+println(lst)
+println(lst.findAll{it > 26})
