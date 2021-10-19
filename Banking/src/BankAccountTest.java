701c5a41f92cd779510b468c5dd83c64e2267381
Joohoon Kim <jayjhkim7@gmail.com>
2021-10-18 21:37:44 -0400

    wip: junit

---
+++ Banking/src/BankAccountTest.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.*;
+
+public class BankAccountTest {
+
+    @org.junit.Test
+    public void deposit() throws Exception {
+        fail("this test yet to be implemented");
+    }
+
+    @org.junit.Test
+    public void withdraw() throws Exception {
+        fail("this test yet to be implemented");
+    }
+
+    @org.junit.Test
+    public void getBalance() throws Exception {
+        fail("this test yet to be implemented");
+    }
+}
